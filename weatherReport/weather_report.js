a756ad19027ac31361b2d5c1f608ee8790c562ec
HenriCanela25 <henritomascanela25@hotmail.com>
2026-03-22 13:01:36 -0400

    Add city parameter

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -2,15 +2,21 @@ function showweatherDetails(event) {
     event.preventDefault();
     const lat = document.getElementById('latitude').value;
     const lon = document.getElementById('longitude').value;
+    const city = document.getElementById('city').value;
     const apiKey = '';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    
+    if (city) {
+        var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    } else {
+        var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    }
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             const weatherInfo = document.getElementById('weatherInfo');
             weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
-                                    <p>Temperature: ${parseFloat(data.main.temp - 273.15).toFixed(2)}</p>
+                                    <p>Temperature: ${data.main.temp}</p>
                                     <p>Weather: ${data.weather[0].description}</p>`;
         })
         .catch(error => {
